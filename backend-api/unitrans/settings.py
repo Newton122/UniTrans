a34a9f5e7f06f1b6d2bea79fc35e21f5dfa66c10
Newton122 <brightonmatic4.com>
2026-06-03 01:49:07 +0100

    Add request body logging middleware

--- backend-api/unitrans/settings.py
+++ backend-api/unitrans/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'unitrans.middleware.LogRequestBodyMiddleware',
 ]
 
 ROOT_URLCONF = 'unitrans.urls'
